d0534edd4f5353df1a2bddc276d365c3e1e25aa5
Naimur Rahman <nrsazib234964@gmail.com>
2025-01-11 00:51:19 +0600

    Added Background on Navbar when scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
-// Sidemenu show hide code
+// Sidemenu show hide
 
 const sideMenu = document.getElementById("sideMenu");
 
-function openMenu(){
+function openMenu() {
     sideMenu.style.transform = 'translateX(-16rem)';
 }
 
-function hideMenu(){
+function hideMenu() {
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+// Add Background on Navbar when scroll
+
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector('nav ul');
+
+window.addEventListener('scroll', () => {
+    if (scrollY > 50) {
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    } else {
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }
+})
